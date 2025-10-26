0f442d2419fcac39fb58f1091e5e2cd04b21d50e
Kendrim87 <226069897+Kendrim87@users.noreply.github.com>
2025-10-26 22:06:35 +0100

    Añadida función actualizar Presupuesto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -4,8 +4,15 @@ let presupuesto = 0;
 // TODO: Funciones adicionales
 
 
-function actualizarPresupuesto() {
-    // TODO
+function actualizarPresupuesto(valor) {
+    // Comprobamos que valor es de tipo number, es >= 0 y no es NaN (que sería tipo number)
+    if (typeof valor === 'number' && valor >= 0 && !isNaN(valor)) {
+        presupuesto = valor;
+        return presupuesto;
+    } else {
+        console.error('Error: El valor introducido no es válido. Debe ser un número que no sea negativo.');
+        return -1;
+    }
 }
 
 function mostrarPresupuesto() {
