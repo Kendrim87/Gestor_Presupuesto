001c11a1ee532dae131da8ad040de7e0eecdb3ac
Kendrim87 <226069897+Kendrim87@users.noreply.github.com>
2025-10-27 10:55:26 +0100

    Añadidas funciones: mostrarPresupuesto() constructor CrearGasto con: 	mostrarGasto() 	actualziarDescripcion(nuevaDescripcion) 	actualizarValor(nuevoValor)

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -3,7 +3,6 @@ let presupuesto = 0;
 
 // TODO: Funciones adicionales
 
-
 function actualizarPresupuesto(valor) {
     // Comprobamos que valor es de tipo number, es >= 0 y no es NaN (que sería tipo number)
     if (typeof valor === 'number' && valor >= 0 && !isNaN(valor)) {
@@ -16,15 +15,41 @@ function actualizarPresupuesto(valor) {
 }
 
 function mostrarPresupuesto() {
-    // TODO
+    // Muestra el presupuesto devuelto por la función actualizarPresupuesto
+    return `Tu presupuesto actual es de ${presupuesto} €`;
 }
 
-function CrearGasto() {
-    // TODO
+class CrearGasto {
+    constructor(descripcion, valor) {
+        // Convertimos la descripción a cadena
+        this.descripcion = String(descripcion);
+        
+        // Validamos que el valor sea un número >= 0
+        if (typeof valor === 'number' && valor >= 0 && !isNaN(valor)) {
+            this.valor = valor;
+        } else {
+            this.valor = 0;
+        }
+    }
+    
+    mostrarGasto() {
+        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+    }
+    
+    actualizarDescripcion(nuevaDescripcion) {
+        this.descripcion = nuevaDescripcion;
+    }
+    
+    actualizarValor(nuevoValor) {
+        // Solo actualizamos si el valor es válido
+        if (typeof nuevoValor === 'number' && nuevoValor >= 0 && !isNaN(nuevoValor)) {
+            this.valor = nuevoValor;
+        }
+    }
 }
 
 // Exportación de funciones
-export   {
+export {
     mostrarPresupuesto,
     actualizarPresupuesto,
     CrearGasto
